70ea7f101db3df42470593668233faca00b44b4b
boclaypool <bo@ipx.org>
2022-08-03 16:08:05 -0500

    more

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 
-    let usersWord = ""
+let oddList = document.getElementById("odd")
+let evenList = document.getElementById("even")
+let usersWord=""
 
-    function updateWord(element) {
-      usersWord = element.value
+function updateWord(element) {
+  usersWord = element.value
+  console.log(usersWord)
+}
 
-      console.log(usersWord)
-    }
+function handleSubmit() {
+  
+    let newListItem = document.createElement('LI')
+        newListItem.innerText = usersWord
+          if (usersWord.length % 2 === 0) 
+          {evenList.appendChild(newListItem)} 
+          else {oddList.appendChild(newListItem)}
+
+    document.getElementById("even-odd-form").reset()
+  }
